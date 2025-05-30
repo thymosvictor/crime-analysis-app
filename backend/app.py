8cefc70c3a47cd74ed1241521aa230767aeaaf2b
Thymos Victor <166143287+thymosvictor@users.noreply.github.com>
2025-05-30 18:51:39 +0000

    Implementa endpoints CRUD básicos para casos

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask import request, abort
 from flask_cors import CORS
 from pymongo import MongoClient
 from dataclasses import dataclass, asdict
@@ -63,6 +64,44 @@ class Caso:
             casos.append(caso.to_dict())
         return casos
     
+    def validar_caso_json(data):
+        try:
+            vitima = data["vitima"]
+            assert isinstance(vitima, dict)
+            assert all(k in vitima for k in ("etnia", "idade"))
+            datetime.fromisoformat(data["data_do_caso"])
+            assert isinstance(data["tipo_do_caso"], str)
+            assert isinstance (data["localizacao"], str)
+        except:
+            return False
+        return True
+
+        @app.route('/api/casos', method=['GET'])
+        def lista_casos():
+            documentos = list(colecao.find({}, {"_id: 0"}))
+            return jsonify(documentos), 200
+        
+        @app.route('/api/casos', methods= ['POST'])
+        def criar_caso():
+            data = request.get_json()
+            if not data or not validar_caso_json(data):
+                abort(400, "JSON inválido ou campos faltando.")
+        colecao.insert_one(data)
+        return jsonify({"message": "Caso criado com sucesso"}), 201
+        
+        @app.route('/api/casos/<string:data_caso>', methods=['GET'])
+        def buscar_caso(data_caso):
+            caso = colecao.find_one({"data_do_caso": data_caso}, {"_id": 0})
+            if not caso:
+                abort(404, "Caso não encontrado.")
+            return jsonify(caso), 200
+        
+        @app.route('/api/casos/<string:data_caso', methods=['DELETE'])
+        def deletar_caso(data_caso):
+            resultado = colecao.delete_one({"data_do_caso": data_caso})
+            if resultado.deleted_count == 0:
+               abort(404, "Caso não encontrado.")
+            return jsonify({"message:" "Caso deletado"}), 200
     if __name__ == "__main__":
         if colecao.count_documents({}) == 0:
             print("Inserindo dados iniciais...")
@@ -70,4 +109,5 @@ class Caso:
             colecao.insert_many(dados_iniciais)
         app.run(debug=True)
 
+    
         
