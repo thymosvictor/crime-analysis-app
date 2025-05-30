db6970c8fb6db0029a929201d6d0fd244c7eb4e2
Thymos Victor <166143287+thymosvictor@users.noreply.github.com>
2025-05-30 17:52:44 +0000

    Adiciona conexão xom MongoDB e modelos de dados

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
-from flask import Flask 
+from flask import Flask, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+from dataclasses import dataclass, asdict
+import random
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
@@ -8,4 +14,60 @@ def hello():
 
 if __name__=='__main__':
     app.run(debug=True)
+
+#MONGODB CONNECTION
+
+MONGO_URI = "mongodb://localhost:27017/"
+client = MongoClient(MONGO_URI)
+db = client["meu_banco"]
+colecao = db["meus_dados"]
+
+@dataclass
+class Vitima:
+    etnia: str
+    idade: int
+
+@dataclass
+class Caso:
+    data_do_caso: str
+    tipo_do_caso: str
+    localizacao: str
+    vitima: Vitima
+
+    def to_dict(self):
+        return {
+             "data_do_caso": self.data_do_caso,
+             "tipo_do_caso": self.tipo_do_caso,
+             "localizacao": self.localizacao,
+             "vitima": asdict(self.vitima)
+        }
     
+    def gerar_dados_aleatorios(n=20):
+        tipos_casos = ["Furto", "Assalto", "Violencia domestica", "Trafico"]
+        locais = ["Centro", "Bairro A", "Bairro B", "Zona Rural"]
+        etnias = ["Branca","Preta","Parda","Indígena","Amarela"]
+        casos = []
+        base_date = datetime.now()
+        for i in range (n):
+            data_caso = (base_date - timedelta(days=random.randint(0, 365))).date().isoformat()
+            caso= Caso(
+                data_do_caso=data_caso,
+                tipo_do_caso=random.choice(tipos_casos),
+                localizacao=random.choice(locais),
+                vitima=Vitima(
+                    etnia=random.choice(etnias),
+                    idade=random.randint(1, 90)
+                )
+
+            )
+            casos.append(caso.to_dict())
+        return casos
+    
+    if __name__ == "__main__":
+        if colecao.count_documents({}) == 0:
+            print("Inserindo dados iniciais...")
+            dados_iniciais = gerar_dados_aleatorios(20)
+            colecao.insert_many(dados_iniciais)
+        app.run(debug=True)
+
+        
